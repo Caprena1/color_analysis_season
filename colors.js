f7a7b0ae0e9c5e2c674ee94927a78d8eaa2375cc
XCA Student <xcastudent@Admins-MBP-2.attlocal.net>
2023-03-14 19:34:32 -0400

    latest changes

--- colors.js
+++ colors.js
@@ -1,16 +1,61 @@
-const form = document.getElementById("form");
-const button = document.getElementById("submitBtn");
+/* const winter = {
+    hair: "black",
+    hairtone: "warm",
+    tan: "yes",
+    palms: "peach",
+    eyes: "deepbrown",
+    jewelry: "gold",
+    sunburn: "no",
+    veins: "greenish",
+    gemstone: "ruby",
+    eyelashes: "black",
+    inside_lip: "peachy",
+    cheeks: "I don't blush, really",
+    skintone: "warm"
+}; */
 
-form.addEventListener('submit', function submitQuiz() {
-    
-    })
-    button.addEventListener("click", form);
-    
+/* const summer = {
+    hair: "hair1",
+    hairtone: "cool",
+    tan: "no",
+    palms: "pink",
+    eyes: "lightblue",
+    jewelry: "silver",
+    sunburn: "yes",
+    veins: "blue_purple",
+    gemstone: "light_green_emerald",
+    eyelashes: "lightblonde",
+    inside_lip: "pinkish",
+    cheeks: "rosypink",
+    skintone: "cool"
+};
+  */
+const button = document.getElementById('submitBtn')
 
 
+button.onclick = function seasonResult() {
+   /*  const form = document.getElementById('form'); */
+    const hair1= document.getElementById('hair1');
+    const hair2 = document.getElementById('hair2');
+    const hair3 = document.getElementById('hair3'); 
+ 
+    if(hair1.checked === true) {
+        alert("Your season is: SPRING");  
+    } else if 
+    (hair2.checked === true) {
+        alert("Your season is: FALL");
+    } else if 
+    (hair3.checked === true) {
+        alert("Your season is: WINTER");       
+        
+    } else
+        alert("You season must be: SUMMER")
+}  
+   /*  if(form === winter) {
+        alert("Your season is: WINTER")
+    }  *//* else if (form === summer) {
+        alert("Your season is: SUMMER")
+    } */
 
-   
 
-   
-    
 
---
+++ season.js
@@ -0,0 +1,1 @@
+const results = document.getElementById("seasonResult").textContent.getItem("textvalue");
